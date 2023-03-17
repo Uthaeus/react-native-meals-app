2a70e328940900f147a865b4bdf15417313b1cd7
Uthaeus <romanlavery@gmail.com>
2023-03-17 10:15:07 -0600

    created mealitem component to display matching meal data on mealoverviewscreen

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,13 +1,22 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, FlatList, StyleSheet } from "react-native";
 
+import MealItem from "../components/MealItem";
 import { MEALS } from "../data/dummy-data";
 
 function MealsOverviewScreen({ route }) {
     const catId = route.params.categorId;
 
+    const displayedMeals = MEALS.filter(mealItem => {
+        return mealItem.categoryIds.indexOf(catId) >= 0;
+    });
+
+    function renderMealItem(itemData) {
+        return <MealItem title={itemData.item.title} />
+    }
+
     return (
         <View style={styles.container}>
-            <Text>Meals overview screen - {catId}</Text>
+            <FlatList data={displayedMeals} keyExtractor={item => item.id} renderItem={renderMealItem} />
         </View>
     );
 }
