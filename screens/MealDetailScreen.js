dc4cdf1314a3eb21c81e48055617504b09c1983a
Uthaeus <romanlavery@gmail.com>
2023-03-17 15:22:02 -0600

    created custom icon component and added to header for mealdetailscreen

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,10 +1,11 @@
 import { useLayoutEffect } from "react";
-import { View, Text, Image, StyleSheet, ScrollView, Button } from "react-native";
+import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
 import List from "../components/MealDetail/List";
 import Subtitle from "../components/MealDetail/Subtitle";
 
 import MealDetails from "../components/MealDetails";
 import { MEALS } from "../data/dummy-data";
+import IconButton from "../components/IconButton";
 
 function MealDetailScreen({ route, navigation }) {
   const mealId = route.params.mealId;
@@ -18,7 +19,7 @@ function MealDetailScreen({ route, navigation }) {
   useLayoutEffect(() => {
     navigation.setOptions({
         headerRight: () => {
-            return <Button title='tap me' onPress={headerButtonPressHandler} />
+            return <IconButton icon='star' color='white' onPress={headerButtonPressHandler} />
         }
     })
   }, [navigation, headerButtonPressHandler])
